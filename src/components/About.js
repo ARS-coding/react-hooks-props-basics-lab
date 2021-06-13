9acf6893f9841ea7ed94a5e6dd2156b096ea4c19
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-13 15:15:27 +0300

    Final product

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Links from "./Links.js";
 
+// Object.keys(props).includes("bio") === true
+
+// props.bio != false
+// Object.keys(props).includes("bio") === true
+// props.bio !== ""
 
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {!!props.bio == true ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={props.github} linkedin={props.linkedin} />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,19 +1,14 @@
 import React from "react";
 
 function Links(props) {
-    console.log(props)
     return (
         <div className="links">
             
             <h1>Links</h1>
 
-            <p>
-                <a href={props.github}>{props.github}</a>
-            </p>
+            <a href={props.github}>{props.github}</a>
 
-            <p>
-                <a href={props.linkedin}>{props.linkedin}</a>
-            </p>
+            <a href={props.linkedin}>{props.linkedin}</a>
         </div>
     )
 }
